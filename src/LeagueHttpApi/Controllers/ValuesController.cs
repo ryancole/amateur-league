dfa506164ab4d696c221708ef70eb8ba2ded4679
Ryan Cole <ryan@rycole.com>
2018-12-21 11:08:45 -0600

    boo

--- src/LeagueHttpApi/Controllers/ValuesController.cs
+++ src/LeagueHttpApi/Controllers/ValuesController.cs
@@ -1,45 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace LeagueHttpApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    [ApiController, Route("values")]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        #region Methods
+
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            var result = new[] { 1, 2, 3 };
+
+            return Ok(result);
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        #endregion
     }
 }
--- src/LeagueHttpApi/Startup.cs
+++ src/LeagueHttpApi/Startup.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace LeagueHttpApi
 {
@@ -20,15 +13,19 @@ namespace LeagueHttpApi
             Configuration = configuration;
         }
 
+        #region Properties
+
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        #endregion
+
+        #region Methods
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -44,5 +41,7 @@ namespace LeagueHttpApi
             app.UseHttpsRedirection();
             app.UseMvc();
         }
+
+        #endregion
     }
 }
