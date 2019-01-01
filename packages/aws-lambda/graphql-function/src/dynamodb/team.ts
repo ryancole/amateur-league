3611bacae81cd8894ce5354209e57fafd7b0d059
Ryan Cole <ryan@rycole.com>
2019-01-01 11:12:19 -0600

    trying to figure out dynamodb

--- packages/aws-lambda/graphql-function/src/dynamodb/team.ts
+++ packages/aws-lambda/graphql-function/src/dynamodb/team.ts
@@ -3,7 +3,13 @@ import AWS from "aws-sdk";
 AWS.config.update({ region: "us-east-2" });
 
 export async function fetchAll() {
-  const client = new AWS.DynamoDB.DocumentClient();
-  const query = await client.query({ TableName: "teams" }).promise();
-  console.log(query.Items);
+  try {
+    const client = new AWS.DynamoDB.DocumentClient({
+      endpoint: "http://localhost:9000"
+    });
+    const query = await client.query({ TableName: "Teams" }).promise();
+    console.log(query.Items);
+  } catch (e) {
+    console.log({ e });
+  }
 }
--- scripts/run-dynamodb.ps1
+++ scripts/run-dynamodb.ps1
@@ -1,6 +1,7 @@
 $directory = "C:\Users\Ryan\AppData\Local\Programs\dynamodb"
+$databasePath = Resolve-Path "$($PSScriptRoot)\.."
 
 Start-Process `
-    -FilePath "java" `
-    -ArgumentList "-Djava.library.path=DynamoDBLocal_lib -jar DynamoDBLocal.jar -inMemory -sharedDb -port 8001" `
-    -WorkingDirectory $directory
+  -FilePath "java" `
+  -ArgumentList "-Djava.library.path=DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb -port 9000 -dbPath $($databasePath)" `
+  -WorkingDirectory $directory
