0b7d9c36e65d55f92d07ee3f1ae132ee22410e1c
Ryan Cole <ryan@rycole.com>
2019-01-02 20:17:56 -0600

    Revert "idk"
    This reverts commit c5987ef33a94ed933481c19e1be72413b94ae077.

--- packages/league-lambda/query-function/src/dynamodb/team.ts
+++
@@ -1,8 +0,0 @@
-import AWS from "aws-sdk";
-
-const documents = new AWS.DynamoDB.DocumentClient({ region: "us-east-2" });
-
-export async function fetchAll() {
-  const response = await documents.scan({ TableName: "Teams" }).promise();
-  return response.Items;
-}
