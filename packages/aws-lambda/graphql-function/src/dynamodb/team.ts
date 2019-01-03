836f065a892edf290580057955c85bedcc7b3d69
Ryan Cole <ryan@rycole.com>
2019-01-02 20:18:18 -0600

    Revert "trying to figure out dynamodb"
    This reverts commit 3611bacae81cd8894ce5354209e57fafd7b0d059.

--- packages/aws-lambda/graphql-function/src/dynamodb/team.ts
+++ packages/aws-lambda/graphql-function/src/dynamodb/team.ts
@@ -3,13 +3,7 @@ import AWS from "aws-sdk";
 AWS.config.update({ region: "us-east-2" });
 
 export async function fetchAll() {
-  try {
-    const client = new AWS.DynamoDB.DocumentClient({
-      endpoint: "http://localhost:9000"
-    });
-    const query = await client.query({ TableName: "Teams" }).promise();
-    console.log(query.Items);
-  } catch (e) {
-    console.log({ e });
-  }
+  const client = new AWS.DynamoDB.DocumentClient();
+  const query = await client.query({ TableName: "teams" }).promise();
+  console.log(query.Items);
 }
--- scripts/run-dynamodb.ps1
+++ scripts/run-dynamodb.ps1
@@ -1,7 +1,6 @@
 $directory = "C:\Users\Ryan\AppData\Local\Programs\dynamodb"
-$databasePath = Resolve-Path "$($PSScriptRoot)\.."
 
 Start-Process `
-  -FilePath "java" `
-  -ArgumentList "-Djava.library.path=DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb -port 9000 -dbPath $($databasePath)" `
-  -WorkingDirectory $directory
+    -FilePath "java" `
+    -ArgumentList "-Djava.library.path=DynamoDBLocal_lib -jar DynamoDBLocal.jar -inMemory -sharedDb -port 8001" `
+    -WorkingDirectory $directory
