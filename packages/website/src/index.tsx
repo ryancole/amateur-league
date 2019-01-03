68eb53b1937abcd5df2df8c67f8fe8a2aa4d65fd
Ryan Cole <ryan@rycole.com>
2019-01-02 20:18:12 -0600

    Revert "time to pick graphql client"
    This reverts commit 3de183d3212ff5c4ce686ec9641d05ed98aecb71.

--- packages/website/src/index.tsx
+++ packages/website/src/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Router from "./router";
 
-ReactDOM.render(<Router />, document.getElementById("container"));
+ReactDOM.render(<p>Hi</p>, document.getElementById("container"));
