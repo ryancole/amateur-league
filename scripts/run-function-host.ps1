d2a256f76b11f782d37b377ed2ceddffccb83d3d
Ryan Cole <ryan@rycole.com>
2019-01-08 17:28:53 -0600

    wut

--- scripts/run-function-host.ps1
+++
@@ -1,7 +0,0 @@
-$funcApplicationPath = "$($env:USERPROFILE)\AppData\Local\AzureFunctionsTools\Releases\2.15.0\cli\func.exe"
-$functionsApplicationBuildPath = Resolve-Path "$($PSScriptRoot)\..\src\LeagueApiFunction\bin\Debug\netcoreapp2.1"
-
-Start-Process `
-  -FilePath $funcApplicationPath `
-  -ArgumentList "host start" `
-  -WorkingDirectory $functionsApplicationBuildPath
