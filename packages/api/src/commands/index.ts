deb459c577ac1901c437fd4430d436c661b520fc
Ryan Cole <ryan@rycole.com>
2019-01-20 13:41:11 -0600

    sda

--- packages/api/src/commands/index.ts
+++ packages/api/src/commands/index.ts
@@ -1,6 +1,6 @@
 import path from "path";
 
-export const handlers = {
+export default {
   "team-get-all": path.resolve(__dirname, "team/get-all"),
   "team-get-single": path.resolve(__dirname, "team/get-single")
 };
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,7 +1,14 @@
 import { Handler } from "aws-lambda";
+import commands from "./commands";
 
 export const handler: Handler = async (event, context) => {
   const body = JSON.parse(event.body);
+
+  // the user specifies which command they'd like to query
+  const { command } = body;
+
+  // we can now get the handler for thr requested command
+  const commandHandler = commands[command];
 
   return {
     body: JSON.stringify(event),
