82e04000d2548c327e89b0a541cd27d18ad20ac6
Ryan Cole <ryan@rycole.com>
2019-01-21 20:20:56 -0600

    rename dir

---
+++ projects/League.Entity/Database/Season.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace League.Entity.Database
+{
+  public class Season
+  {
+    #region Properties
+
+    public long Id { get; set; }
+
+    public DateTime DateCreated { get; set; }
+
+    #endregion
+  }
+}
